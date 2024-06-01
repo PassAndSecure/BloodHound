97c86df44dcb660471f9f3ebd0a98832852bfa47
Pass And Secure <161962973+PassAndSecure@users.noreply.github.com>
2024-06-01 21:45:18 +0200

    Update Download_ AzureHound.ps1

--- Download_ AzureHound.ps1
+++ Download_ AzureHound.ps1
@@ -1,4 +1,4 @@
-# Selection dossier
+# selection dossier
 function Select-FolderDialog {
     Add-Type -AssemblyName System.Windows.Forms
     $folderBrowser = New-Object System.Windows.Forms.FolderBrowserDialog
@@ -6,38 +6,38 @@ function Select-FolderDialog {
     return $folderBrowser.SelectedPath
 }
 
-# URL github
-$baseUrl = "https://raw.githubusercontent.com/PassAndSecure/BloodHound/main/AzureHound/"
+# URL dépôt GitHub
+$baseUrl = "https://github.com/PassAndSecure/BloodHound/archive/refs/heads/main.zip"
 
-# fichiers a télécharger
-$files = @(
-    "AzureHound.ps1",
-    "AzureHound.exe",
-    "AzureHound.exe.config",
-    "AzureHound.pdb",
-    "System.Console.dll",
-    "System.Diagnostics.Tracing.dll",
-    "System.Net.Http.dll"
-)
-
+# selection de dossier
 $destinationFolder = Select-FolderDialog
 
 if ($destinationFolder -ne "") {
-    # Créer dossier AzureHound dans dossier sélectionné
+    # Créer dossier AzureHound 
     $azureHoundFolder = Join-Path -Path $destinationFolder -ChildPath "AzureHound"
     if (-Not (Test-Path -Path $azureHoundFolder)) {
         New-Item -ItemType Directory -Path $azureHoundFolder -Force
     }
 
-    # Télécharge fichiers dans AzureHound
-    foreach ($file in $files) {
-        $url = $baseUrl + $file
-        $output = Join-Path -Path $azureHoundFolder -ChildPath $file
-        Write-Host "Téléchargement de $file..."
-        Invoke-WebRequest -Uri $url -OutFile $output
-    }
+    # Télécharger zip dépôt GitHub
+    $zipFilePath = Join-Path -Path $destinationFolder -ChildPath "BloodHound-main.zip"
+    Write-Host "Téléchargement du dépôt GitHub..."
+    Invoke-WebRequest -Uri $baseUrl -OutFile $zipFilePath
 
-    Write-Host "Téléchargement terminé ! Les fichiers ont été téléchargés dans $azureHoundFolder."
+    # Extraire zip dans dossier destination
+    Write-Host "Extraction des fichiers..."
+    Add-Type -AssemblyName System.IO.Compression.FileSystem
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipFilePath, $destinationFolder)
+
+    # Déplacer dossiers AzureHound extraits vers dossier destination
+    $extractedFolderPath = Join-Path -Path $destinationFolder -ChildPath "BloodHound-main/AzureHound"
+    Move-Item -Path "$extractedFolderPath/*" -Destination $azureHoundFolder -Force
+
+    # Delete zip et dossier extrait
+    Remove-Item -Path $zipFilePath -Force
+    Remove-Item -Path (Join-Path -Path $destinationFolder -ChildPath "BloodHound-main") -Recurse -Force
+
+    Write-Host "Téléchargement et extraction terminés ! Les fichiers ont été téléchargés dans $azureHoundFolder."
 } else {
     Write-Host "Aucun dossier sélectionné. Opération annulée."
 }
